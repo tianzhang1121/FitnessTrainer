6a4455c4d8619f2e0cbc291d31b911f909d954ca
tianzhang1121 <tyz@utexas.edu>
2015-11-27 19:28:14 -0600

    2

--- app/src/main/java/com/final_project_386w/fitnesstrainer/Evaluate.java
+++ app/src/main/java/com/final_project_386w/fitnesstrainer/Evaluate.java
@@ -8,63 +8,97 @@ import android.content.res.AssetManager;
 import java.io.*;
 import android.util.Log;
 import weka.classifiers.*;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.*;
+import android.os.*;
+import java.util.*;
 
 public class Evaluate extends AppCompatActivity {
 
     TextView loading;
     Button back_button, main_button;
-
+    private Handler handler = new Handler();
+    String model_path = android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/Trainer_Models/";
+    ArrayList<Attribute> attrs = new ArrayList<Attribute>();
+    ArrayList<String> attr_class = new ArrayList<String>();
+    double interval = 3.5;
+    List<Float> x_vals;
+    List<Float> y_vals;
+    List<Float> z_vals;
+    String model_file;
+    String interval_file;
+    Bundle extras;
+    Instances data_set;
+    Instances labeled_set;
+    int inst_num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluate);
-        copyAssets();
-    }
-    private void copyAssets() {
-        AssetManager assetManager = getAssets();
-        String[] files = null;
-        try {
-            files = assetManager.list("");
-        } catch (IOException e) {
-            Log.e("tag", "Failed to get asset file list.", e);
+        loading = (TextView) findViewById(R.id.statistics);
+        for (int i = 0; i < (int) (interval * 1000 / 100); ++i) {
+            attrs.add(new Attribute("x" + i));
+            attrs.add(new Attribute("y" + i));
+            attrs.add(new Attribute("z" + i));
         }
-        if (files != null) for (String filename : files) {
-            InputStream in = null;
-            OutputStream out = null;
+        attr_class.add("correct");
+        attr_class.add("incorrect");
+        attrs.add(new Attribute("category", attr_class));
+        extras = getIntent().getExtras();
+        x_vals = toList(extras.getFloatArray("x_vals"));
+        y_vals = toList(extras.getFloatArray("y_vals"));
+        z_vals = toList(extras.getFloatArray("z_vals"));
+        Log.d("SIZE!!!!!!!!!", ((Integer) x_vals.size()).toString());
+
+        model_file = extras.getString("model_file");
+        interval_file = extras.getString("interval_file");
+        handler.postDelayed(evaluate, 0);
+    }
+    public Runnable evaluate = new Runnable() {
+        public void run() {
             try {
-                //if(filename == "bayes_mod.model") {
-                    in = assetManager.open(filename);
-                    File outFile = new File(getExternalFilesDir(null), filename);
-                    out = new FileOutputStream(outFile);
-                    copyFile(in, out);
-               // }
-            } catch(IOException e) {
-                Log.e("tag", "Failed to copy asset file: " + filename, e);
-            }
-            finally {
-                if (in != null) {
-                    try {
-                        in.close();
-                    } catch (IOException e) {
-                        // NOOP
+                // Create an empty training set
+                inst_num = (int) ((x_vals.size() * 100)/(interval * 1000));
+                data_set = new Instances("Accelerometer", attrs, inst_num);
+                // Set class index
+                data_set.setClassIndex(attrs.size()-1);
+
+                for(int i = 0; i < inst_num; ++i ) {
+                    Instance gesture = new DenseInstance(attrs.size());
+                    gesture.setDataset(data_set);
+                    for(int j = 0; j < attrs.size()-1; j += 3) {
+                        gesture.setValue((Attribute) attrs.get(j), x_vals.get(j/3));
+                        gesture.setValue((Attribute) attrs.get(j+1), y_vals.get(j/3));
+                        gesture.setValue((Attribute) attrs.get(j+2), z_vals.get(j/3));
                     }
+                    gesture.setClassMissing();
+                    //gesture.setDataset(data_set);
+                    //gesture.setValue((Attribute) attrs.get(attrs.size()-1), "?");
+                    // add the instance
+                    data_set.add(gesture);
                 }
-                if (out != null) {
-                    try {
-                        out.close();
-                    } catch (IOException e) {
-                        // NOOP
-                    }
+
+                Classifier cModel = (Classifier) weka.core.SerializationHelper.read(model_path + model_file);
+                labeled_set = new Instances(data_set);
+
+                for (int i = 0; i < data_set.numInstances(); i++) {
+                    double clsLabel = cModel.classifyInstance(data_set.instance(i));
+                    labeled_set.instance(i).setClassValue(clsLabel);
+                    Log.d("INSTANCE", labeled_set.instance(i).toString(attrs.size()-1));
                 }
+                //Log.d("RESULT", labeled_set.toSummaryString());
+                //loading.setText(labeled_set.toSummaryString());
+
+            } catch(Exception e){
+                Log.e("MYAPP", "exception", e);
             }
         }
-    }
-    private void copyFile(InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[1024];
-        int read;
-        while((read = in.read(buffer)) != -1){
-            out.write(buffer, 0, read);
+    };
+    public static List<Float> toList(float[] in){
+        List<Float> result = new ArrayList<Float>(in.length);
+        for(float f : in){
+            result.add(f);
         }
+        return result;
     }
-    //Classifier cModel = (Classifier) SerializationHelper.read();
 }
--- app/src/main/java/com/final_project_386w/fitnesstrainer/Train.java
+++ app/src/main/java/com/final_project_386w/fitnesstrainer/Train.java
@@ -16,6 +16,8 @@ import android.view.ViewGroup.LayoutParams;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.widget.*;
+import java.io.*;
+import android.util.*;
 
 
 public class Train extends AppCompatActivity implements SensorEventListener{
@@ -31,27 +33,49 @@ public class Train extends AppCompatActivity implements SensorEventListener{
     private ToneGenerator tone;
     private SensorManager sensor_manager;
     private Sensor accelerometer;
-    Vector x_vals = new Vector();
-    Vector y_vals = new Vector();
-    Vector z_vals = new Vector();
+    private List<List<Float>> x_list = new ArrayList<>();
+    private List<List<Float>> y_list = new ArrayList<>();
+    private List<List<Float>> z_list = new ArrayList<>();
+    private List<Float> x_vals = new ArrayList<>();
+    private List<Float> y_vals = new ArrayList<>();
+    private List<Float> z_vals = new ArrayList<>();
+
+    private FileDialog fileDialog;
+    private String model_file;
+    private String interval_file;
+    private int sensor_ms = 0;
+    private long sensor_time = 0L;
+    private long sensor_start_time = 0L;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train);
-
+        File mPath = new File(Environment.getExternalStorageDirectory() + "/Trainer_Models/");
+        fileDialog = new FileDialog(this, mPath);
+        //fileDialog.setFileEndsWith(".model");
+        fileDialog.addFileListener(new FileDialog.FileSelectedListener() {
+            public void fileSelected(File file) {
+                //Log.d(getClass().getName(), "selected file " + file.toString());
+                //Log.d(getClass().getName(), "selected file " + file.getName());
+                model_file = file.getName();
+                interval_file = model_file.substring(0, model_file.length()-6) + ".txt";
+                //Log.d(getClass().getName(), "selected file " + interval_file);
+            }
+        });
+        fileDialog.showDialog();
         tone = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
         start_button = (Button) findViewById(R.id.start_button);
         evaluate_button = (Button) findViewById(R.id.evaluate_button);
         time_text = (TextView) findViewById(R.id.timer);
         sensor_manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        load_button = (Button) findViewById(R.id.load_button);
 
         if (sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
 
             accelerometer = sensor_manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensor_manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
-        } else {
+            sensor_manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
 
         }
 
@@ -62,13 +86,13 @@ public class Train extends AppCompatActivity implements SensorEventListener{
                             x_vals.clear();
                             y_vals.clear();
                             z_vals.clear();
+                            mode = 0;
                             start_button.setText("Stop");
-                            start_time = SystemClock.uptimeMillis();
+                            start_time = SystemClock.uptimeMillis() + 5000L;
+                            sensor_start_time = SystemClock.uptimeMillis() + 5000L;
                             handler.postDelayed(update_timer, 5000);
                             handler.postDelayed(make_beep, 5000);
-                            mode = 0;
                         } else {
-                            milliseconds = 0;
                             start_button.setText("Start");
                             time_text.setText("00:00:00");
                             time_text.setTextColor(Color.BLACK);
@@ -85,26 +109,55 @@ public class Train extends AppCompatActivity implements SensorEventListener{
             new View.OnClickListener() {
                 public void onClick(View v) {
                     Intent i = new Intent(Train.this, Evaluate.class);
+                    Bundle extras = new Bundle();
+                    extras.putFloatArray("x_vals", toArray(x_vals));
+                    extras.putFloatArray("y_vals", toArray(y_vals));
+                    extras.putFloatArray("z_vals", toArray(z_vals));
+                    extras.putString("model_file", model_file);
+                    extras.putString("interval_file", interval_file);
+                    i.putExtras(extras);
                     startActivity(i);
                 }
             }
         );
 
+        load_button.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        fileDialog.addFileListener(new FileDialog.FileSelectedListener() {
+                            public void fileSelected(File file) {
+                                //Log.d(getClass().getName(), "selected file " + file.toString());
+                                //Log.d(getClass().getName(), "selected file " + file.getName());
+                                model_file = file.getName();
+                                interval_file = model_file.substring(0, model_file.length()-6) + ".txt";
+                                //Log.d(getClass().getName(), "selected file " + interval_file);
+                            }
+                        });
+                        fileDialog.showDialog();
+                    }
+                }
+        );
+
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        if(milliseconds != 0){
-            if((milliseconds % 100) == 0){
-                x_vals.addElement(event.values[0]);
-                y_vals.addElement(event.values[1]);
-                z_vals.addElement(event.values[2]);
+        if(mode == 0){
+            sensor_time = SystemClock.uptimeMillis() - sensor_start_time;
+            sensor_ms = (int) (sensor_time % 1000);
+            if(sensor_time >= 50){
+                Log.d("!!!!!", ((Integer) (sensor_ms)).toString());
+
+                x_vals.add(event.values[0]);
+                y_vals.add(event.values[1]);
+                z_vals.add(event.values[2]);
+                sensor_start_time = sensor_time;
             }
         }
 
@@ -115,7 +168,7 @@ public class Train extends AppCompatActivity implements SensorEventListener{
             time_ms = SystemClock.uptimeMillis() - start_time;
             secs = (int) (time_ms / 1000);
             mins = secs / 60;
-            secs = (secs % 60) - 5;
+            secs = (secs % 60);
             milliseconds = (int) (time_ms % 1000);
             time_text.setText("" + mins + ":" + String.format("%02d", secs) + ":"
                     + String.format("%02d", milliseconds));
@@ -126,8 +179,19 @@ public class Train extends AppCompatActivity implements SensorEventListener{
     public Runnable make_beep = new Runnable() {
         public void run() {
             tone.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
-            handler.postDelayed(this, 5000);
+            //TODO
+            handler.postDelayed(this, 3500);
         }
     };
+
+
+    //SOF
+    public static float[] toArray(List<Float> in){
+        float[] result = new float[in.size()];
+        for(int i=0; i<result.length; i++){
+            result[i] = in.get(i);
+        }
+        return result;
+    }
 
 }
